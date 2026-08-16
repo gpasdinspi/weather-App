63ba17f5735e1a02ee5f4ac799521abd42f4e51f
gpasdinspi <154826194+gpasdinspi@users.noreply.github.com>
2026-08-16 18:45:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Nom du projet
+
+Courte description du projet.
+
+## Fonctionnalités
+- 
+
+## Installation
+npm install --save-dev webpack webpack-cli webpack-dev-server webpack-merge html-webpack-plugin css-loader style-loader eslint eslint-config-prettier prettier gh-pages
+npm run dev
+
+## Build
+\`\`\`bash
+npm run build
+\`\`\`
+
+## Aperçu
+(screenshot ou lien vers la démo)
---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Mon Projet',
+      template: './src/index.html',
+    }),
+  ],
+};
