d42c364ea5a0b193923961827445e93f70d90e0e
Your Name <you@example.com>
2026-08-22 13:06:36 +0200

    created the functions to display elements and added a basic ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import './styles/reset.css';
+import sun from "./img/sun.png";
+import snow from "./img/snow.png";
+import rain from "./img/rainy-day.png";
+import clouds from "./img/clouds.png";
+import weather from "./img/weather.png";
+let symbol = "°F";
 
 async function getWeather(city) {
     let respons = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=EDNQHTB9LK9ZHRDANNKXY8EC9`)
-    let data = respons.json();
+    let data = await respons.json();
     console.log(data);
     return data;
 }
@@ -16,13 +22,82 @@ function filterJson(data){
     }
 }
 
+function getGif(temp) {
+    temp = temp.toLowerCase();
+    if (temp.includes("snow")){
+        return snow;
+    }
+    if (temp.includes("rain")){
+        return rain;
+    }
+    if (temp.includes("clear")){
+        return sun;
+    }
+    if (temp.includes("partly-cloudy-day")){
+        return weather;
+        }
+    return clouds;
+}
+
+function displayWeather(data){
+    let container = document.querySelector(".display");
+    let html = `
+     <div class="today">
+            <div class="L1">
+                <H4>
+                    ${data.adress}
+                </H4>
+                <div>
+                    <p>°C</p> | <p>°F</p>
+                </div>
+            </div>
+            <div class="info">
+                <div class="temp">
+                    <img id="big-logo" src=${getGif(data.currentCondition.icon)}>
+                    ${data.currentCondition.temp } ${symbol}
+                </div>
+                <div class="details">
+                <p>condition : ${data.currentCondition.conditions} </p>
+                <p>humidity : ${data.currentCondition.humidity} </p>
+                <p>sunrise : ${data.currentCondition.sunrise} </p>
+                <p>sunset : ${data.currentCondition.sunset} </p>
+
+                </div>
+            </div>
+        <hr>
+        </div>
+        <div class="nextDays">
+        `
+        for (const element of data.days.slice(1)) {
+            html += `
+             <div class="day">
+                <h5>${element.datetime}</h5>
+                <img id="logo" src=${getGif(element.icon)}>
+                <p>${element.tempmax} ${symbol} ${element.tempmin} ${symbol}</p>
+            </div>
+            `
+        }
+        html += `
+        </div>
+        <hr>
+        <div class="description">
+        
+            ${data.description}
+        
+        </div>
+        <hr>
+    `
+    container.innerHTML = html;
+}
+
 (function main(){
     let btn = document.querySelector(".city-btn");
     btn.addEventListener("click", (e)=>{
         e.preventDefault();
         let city = document.getElementById("city").value || "london";
         getWeather(city).then(r => {
-            console.log(filterJson(r))
+            console.log(filterJson(r));
+            displayWeather(filterJson(r));
         })
     })
     
